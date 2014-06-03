b1c9b2a6038a7dab9a8fe455094bda2048f05e3d
cnsworder <cnsworder@gmail.com>
2014-06-03 18:08:50 +0800

    Modify: 初始化项目

---
+++ gam.py
@@ -0,0 +1,7 @@
+#!env python2
+
+from gam import *
+
+if __name__ == "__main__":
+    am = app_manager.AppManager();
+    am.update()
---
+++ gam/app_manager.py
@@ -0,0 +1,6 @@
+class AppManager:
+    def __init__(self):
+        pass
+
+    def update(self):
+        print("Update")
