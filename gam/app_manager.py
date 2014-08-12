a3861e4862fe64b0ea70942a277717f7f3c5d286
cnsworder <cnsworder@gmail.com>
2014-08-12 18:49:00 +0800

    Modify: conf files

--- gam/app_manager.py
+++ gam/app_manager.py
@@ -5,11 +5,27 @@ from gam import git_manager
 
 class AppManager:
     def __init__(self):
-        pass
+        self.app_list = []
 
     def update(self):
+        """
+        check update from github
+        """
         git = git_manager.GitManager()
         if git.check_update():
             git.update()
         else:
             print("No have updated!")
+    
+    def read_app_list(self):
+        """
+        Read app list from conf/applists.conf
+        """
+        print("Read app list from conf/applists.conf")
+        self.app_list.append("")
+
+    def install_app_list(self):
+        """
+        Install application from list, install program in conf/<application name>.conf
+        """
+        print("Install application from list")
